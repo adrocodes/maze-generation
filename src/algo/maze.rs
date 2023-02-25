d2eb11f98711b19444b4fd969afccf904bb4ee7a
adrocodes <adrocodes@users.noreply.github.com>
2023-02-25 11:44:58 +1100

    feat: setup basic maze structs and impl

--- src/algo/maze.rs
+++ src/algo/maze.rs
@@ -1,3 +1,51 @@
 pub trait MazeGenerate {
     fn generate(self) -> Self;
 }
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
+pub struct Cell {
+    pub x: usize,
+    pub y: usize,
+    /// [top, right, bottom, left]
+    pub walls: [bool; 4],
+}
+
+impl Cell {
+    fn new(x: usize, y: usize) -> Self {
+        Self {
+            x,
+            y,
+            walls: [true, true, true, true],
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Grid {
+    pub cols: usize,
+    pub rows: usize,
+    pub matrix: Vec<Vec<Cell>>,
+}
+
+impl Grid {
+    fn build_matrix(rows: usize, cols: usize) -> Vec<Vec<Cell>> {
+        let mut matrix = Vec::<Vec<Cell>>::with_capacity(rows);
+        for y in 0..rows {
+            let mut row = Vec::<Cell>::with_capacity(cols);
+
+            for x in 0..cols {
+                row.push(Cell::new(x, y));
+            }
+
+            matrix.push(row);
+        }
+
+        matrix
+    }
+
+    pub fn new(rows: usize, cols: usize) -> Self {
+        let matrix = Grid::build_matrix(rows, cols);
+
+        Self { rows, cols, matrix }
+    }
+}
