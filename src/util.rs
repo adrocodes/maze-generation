5ed1a4732c22a914a281aaf236771841342e1521
adrocodes <adrocodes@users.noreply.github.com>
2023-03-01 19:49:12 +1100

    test: minor offset tests

--- src/util.rs
+++ src/util.rs
@@ -41,3 +41,44 @@ pub fn build_offset_getter<T: PartialEq + Sub<u32, Output = T> + Add<u32, Output
         Some(index_tuple)
     }
 }
+
+mod test {
+    use super::build_offset_getter;
+    use crate::algo::maze::Direction;
+
+    #[test]
+    fn test_top_corner() {
+        let get = build_offset_getter((0, 0), (10, 10));
+
+        let top = get(0, 0, Direction::Top);
+        assert!(top.is_none());
+
+        let right = get(0, 0, Direction::Right);
+        assert!(right.is_some());
+        assert_eq!((0, 1), right.unwrap());
+
+        let bottom = get(0, 0, Direction::Bottom);
+        assert!(bottom.is_some());
+        assert_eq!((1, 0), bottom.unwrap());
+
+        let left = get(0, 0, Direction::Left);
+        assert!(left.is_none());
+    }
+
+    #[test]
+    fn test_all_dirs_corner() {
+        let get = build_offset_getter((0, 0), (10, 10));
+
+        let top = get(4, 4, Direction::Top);
+        assert_eq!((3, 4), top.unwrap());
+
+        let right = get(4, 4, Direction::Right);
+        assert_eq!((4, 5), right.unwrap());
+
+        let bottom = get(4, 4, Direction::Bottom);
+        assert_eq!((5, 4), bottom.unwrap());
+
+        let left = get(4, 4, Direction::Left);
+        assert_eq!((4, 3), left.unwrap());
+    }
+}
