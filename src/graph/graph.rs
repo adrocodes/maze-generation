5acd2b5939a042f891d12df6371bad3337af9acb
adrocodes <adrocodes@users.noreply.github.com>
2023-03-08 19:46:41 +1100

    feat: build path during search

--- src/graph/graph.rs
+++ src/graph/graph.rs
@@ -8,7 +8,6 @@ use std::{
 #[derive(Debug)]
 pub struct Graph<T: PartialEq + Eq + Hash + Clone + Debug> {
     pub vertices: HashMap<T, HashSet<T>>,
-    pub vertices_v2: Vec<Node<T>>,
 }
 
 #[derive(Debug)]
@@ -19,12 +18,14 @@ pub struct Node<T: PartialEq + Eq + Hash + Clone + Debug> {
 }
 
 impl<T: PartialEq + Eq + Hash + Clone + Debug> Graph<T> {
-    pub fn bfs(&self, start: T, end: T) -> Option<Node<T>> {
-        let mut stack = VecDeque::<Node<T>>::new();
+    pub fn bfs(&self, start: T, end: T) -> Option<(T, Vec<Node<T>>)> {
+        let mut stack = VecDeque::<T>::new();
         let mut visited = HashSet::<T>::new();
+        let mut path = Vec::<Node<T>>::new();
 
         visited.insert(start.clone());
-        stack.push_front(Node {
+        stack.push_front(start.clone());
+        path.push(Node {
             value: start,
             parent: None,
             children: None,
@@ -32,16 +33,17 @@ impl<T: PartialEq + Eq + Hash + Clone + Debug> Graph<T> {
 
         while !stack.is_empty() {
             if let Some(v) = stack.pop_front() {
-                if v.value == end {
-                    return Some(v);
+                if v == end {
+                    return Some((v, path));
                 } else {
-                    if let Some(edges) = self.vertices.get(&v.value) {
+                    if let Some(edges) = self.vertices.get(&v) {
                         for w in edges.iter() {
                             if !visited.contains(&w) {
                                 visited.insert(w.clone());
-                                stack.push_back(Node {
+                                stack.push_back(w.clone());
+                                path.push(Node {
                                     value: w.clone(),
-                                    parent: Some(v.value.clone()),
+                                    parent: Some(v.clone()),
                                     children: None,
                                 });
                             }
