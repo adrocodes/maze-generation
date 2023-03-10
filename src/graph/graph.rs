a7b383b2f018cb5bfc52bc2e20600280ce6efce5
adrocodes <adrocodes@users.noreply.github.com>
2023-03-11 10:11:49 +1100

    feat: switch path to a hashmap

--- src/graph/graph.rs
+++ src/graph/graph.rs
@@ -18,34 +18,40 @@ pub struct Node<T: PartialEq + Eq + Hash + Clone + Debug> {
 }
 
 impl<T: PartialEq + Eq + Hash + Clone + Debug> Graph<T> {
-    pub fn bfs(&self, start: T, end: T) -> Option<Vec<Node<T>>> {
+    pub fn bfs(&self, start: T, end: T) -> Option<HashMap<T, Node<T>>> {
         let mut stack = VecDeque::<T>::new();
         let mut visited = HashSet::<T>::new();
-        let mut path = Vec::<Node<T>>::new();
+        let mut map_path = HashMap::<T, Node<T>>::new();
 
         visited.insert(start.clone());
         stack.push_front(start.clone());
-        path.push(Node {
-            value: start,
-            parent: None,
-            children: None,
-        });
+        map_path.insert(
+            start.clone(),
+            Node {
+                value: start.clone(),
+                parent: None,
+                children: None,
+            },
+        );
 
         while !stack.is_empty() {
             if let Some(v) = stack.pop_front() {
                 if v == end {
-                    return Some(path);
+                    return Some(map_path);
                 } else {
                     if let Some(edges) = self.vertices.get(&v) {
                         for w in edges.iter() {
                             if !visited.contains(&w) {
                                 visited.insert(w.clone());
                                 stack.push_back(w.clone());
-                                path.push(Node {
-                                    value: w.clone(),
-                                    parent: Some(v.clone()),
-                                    children: None,
-                                });
+                                map_path.insert(
+                                    w.clone(),
+                                    Node {
+                                        value: w.clone(),
+                                        parent: Some(v.clone()),
+                                        children: None,
+                                    },
+                                );
                             }
                         }
                     }
